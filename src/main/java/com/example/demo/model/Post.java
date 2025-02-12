a534b5f6740c1cc08d75c27fdecc5da34fd7f329
ItsKil0Byte <itskilobyte@gmail.com>
2025-02-12 13:30:47 +0500

    added a database support to project

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -1,17 +1,29 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
 import java.util.Date;
 
-public class Post {
-    private String text;
-    private Integer likes;
-    private Date creationDate;
-    private Long id;
 
-    public Post(Long id, String text, Date creationDate) {
+@Entity
+public class Post implements Serializable {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String text;
+    private Integer likes = 0;
+    private Date creationDate;
+
+    public Post(Long id, String text, Date date) {
         this.id = id;
         this.text = text;
-        this.creationDate = creationDate;
+        this.creationDate = date;
+    }
+
+    public Post() {
     }
 
     public Integer getLikes() {
--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -1,34 +1,26 @@
 package com.example.demo.service;
 
 import com.example.demo.model.Post;
+import com.example.demo.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.StreamSupport;
 
 @Service
 public class PostService {
 
-    private List<Post> posts;
-
-    {
-        Post first = new Post(0L, "Мой первый пост :)", new Date());
-        Post second = new Post(1L, "В интернете весело :)", new Date());
-        Post third = new Post(2L, "Я люблю Java.", new Date());
-
-        first.setLikes(11);
-        second.setLikes(4);
-        third.setLikes(7);
-
-        posts = new ArrayList<>(List.of(first, second, third));
-    }
+    @Autowired
+    PostRepository postRepository;
 
     public void create(String text) {
-        posts.add(new Post((long) posts.size(), text, new Date()));
+        Post post = new Post(null, text, new Date());
+        postRepository.save(post);
     }
 
     public List<Post> listAllPosts() {
-        return posts;
+        return StreamSupport.stream(postRepository.findAll().spliterator(), false).toList();
     }
 }
